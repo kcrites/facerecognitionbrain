fbf42146c557056d9faaef33e8fb87a63ebce557
Ken Crites <kenrcrites@gmail.com>
2019-01-03 17:57:50 +0100

    incremental updates to include API call, signin form, and bounding boxes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
+import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '295384f299ec406ba056982f91bcc644'
+});
 
 const particlesOptions = {
               
@@ -22,6 +29,60 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin'
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage');
+    const height = Number(image.height);
+    const width = Number(image.width);
+    console.log(height, width);
+    console.log(data.outputs[0].data.regions[0].region_info.bounding_box)
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+   this.setState({input: event.target.value});
+
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input} );
+
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+    
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,11 +90,16 @@ class App extends Component {
             params={particlesOptions}
             />
   
-        <Navigation />
-       <Logo />
-       <Rank />
-        <ImageLinkForm />
-             { /* <FaceRecognition />*/}
+        <Navigation onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'signin'
+        ?  <Signin onRouteChange={this.onRouteChange}/>
+        :  <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+            </div>
+      }
       </div>
     );
   }
